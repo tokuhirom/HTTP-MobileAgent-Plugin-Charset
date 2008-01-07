59982155c667f0575865cc6ae69f07ac655adcc3
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-07 14:35:18 +0000

    H::MA::Plugin-Charset: implemented.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAgent-Plugin-Charset/trunk@4189 d0d07461-0603-4401-acd4-de1884942a52

--- lib/HTTP/MobileAgent/Plugin/Charset.pm
+++ lib/HTTP/MobileAgent/Plugin/Charset.pm
@@ -3,25 +3,29 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
-# -------------------------------------------------------------------------
-# docomo
-sub HTTP::MobileAgent::DoCoMo::encoding {
-    my $self = shift;
-    $self->can_display_utf8 ? 'x-utf8-docomo' : 'x-sjis-docomo';
-}
-# http://www.nttdocomo.co.jp/service/imode/make/content/xhtml/about/
-sub HTTP::MobileAgent::DoCoMo::can_display_utf8 {
+sub HTTP::MobileAgent::can_display_utf8 {
     my $self = shift;
     $self->xhtml_compliant;
 }
+sub HTTP::MobileAgent::AirHPhone::can_display_utf8 {
+    my $self = shift;
+    $self->get_header('Accept-Charset') =~ /utf-?8/i ? 1 : 0;
+}
 
-# -------------------------------------------------------------------------
-# non-mobile
-sub HTTP::MobileAgent::NonMobile::encoding { 'utf8' }
-sub HTTP::MobileAgent::NonMobile::can_display_utf8 { 1 }
+sub HTTP::MobileAgent::encoding {
+    my $self = shift;
+    if ($self->is_non_mobile) {
+        return 'utf-8';
+    } else {
+        my $charset = $self->can_display_utf8 ? 'utf8' : 'sjis';
+        return join '-', 'x', $charset, lc($self->carrier_longname);
+    }
+}
 
 1;
 __END__
+
+=encoding utf8
 
 =for stopwords aaaatttt dotottto gmail
 
@@ -36,7 +40,7 @@ HTTP::MobileAgent::Plugin::Charset - Encode::JP::Mobile friendly
 
   my $agent = HTTP::MobileAgent->new;
   $agent->can_display_utf8; # => 1 or 0
-  decode($agent->encoding, $r->param('body'));
+  encode($agent->encoding, "\x{223e}");
 
 =head1 DESCRIPTION
 
@@ -51,6 +55,9 @@ Tokuhiro Matsuno E<lt>tokuhirom aaaatttt gmail dotottto commmmmE<gt>
 =head1 SEE ALSO
 
 L<HTTP::MobileAgent>, L<Encode::JP::Mobile>
+
+L<http://www.au.kddi.com/ezfactory/tec/spec/wap_rule.html>
+L<http://www.nttdocomo.co.jp/service/imode/make/content/xhtml/about/>
 
 =head1 LICENSE
 
--- t/01_can_display_utf8.t
+++ t/01_can_display_utf8.t
@@ -16,6 +16,13 @@ sub can_display_utf8 {
     my $ua = shift;
 
     local $ENV{HTTP_USER_AGENT} = $ua;
+    local $ENV{HTTP_ACCEPT_CHARSET};
+    if ($ua =~ /DDIPOCKET/) {
+        $ENV{HTTP_ACCEPT_CHARSET} =
+          ( $ua =~ /K3001V/ )
+          ? 'shift_jis, utf-8, utf-16, iso-8859-1;q=0.6, *;q=0.1'
+          : 'shift_jis; q=0.6, *;q=0.1';
+    }
     my $agent = HTTP::MobileAgent->new;
     $agent->can_display_utf8 ? 'utf8' : 'no utf8';
 }
@@ -33,4 +40,28 @@ __END__
 === docomo mova
 --- input: DoCoMo/1.0/D501i
 --- expected: no utf8
+
+=== 
+--- input: Vodafone/1.0/V802SE/SEJ001/SNXXXXXXXXX Browser/SEMC-Browser/4.1 Profile/MIDP-2.0 Configuration/CLDC-1.10
+--- expected: utf8
+
+===
+--- input: J-PHONE/2.0/J-DN02
+--- expected: no utf8
+
+=== willcom
+--- input: Mozilla/3.0(DDIPOCKET;JRC/AH-J3001V,AH-J3002V/1.0/0100/c50)CNF/2.0
+--- expected: no utf8
+
+=== willcom
+--- input: Mozilla/3.0(DDIPOCKET;KYOCERA/AH-K3001V/1.4.1.67.000000/0.1/C100) Opera 7.0
+--- expected: utf8
+
+=== ez
+--- input: UP.Browser/3.01-HI01 UP.Link/3.4.5.2
+--- expected: no utf8
+
+=== ez
+--- input: KDDI-TS21 UP.Browser/6.0.2.276 (GUI) MMP/1.1
+--- expected: utf8
 
