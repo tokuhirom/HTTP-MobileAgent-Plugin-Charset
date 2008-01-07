537ded1f5c22d396e3982d477ecab7484e3f8fa9
tokuhirom <tokuhirom@d0d07461-0603-4401-acd4-de1884942a52>
2008-01-07 07:49:37 +0000

    HTTP-MobileAgent-Plugin-Charset: added skelton.
    git-svn-id: http://svn.coderepos.org/share/lang/perl/HTTP-MobileAgent-Plugin-Charset/trunk@4173 d0d07461-0603-4401-acd4-de1884942a52

---
+++ blib/lib/HTTP/MobileAgent/Plugin/Charset.pm
@@ -0,0 +1,33 @@
+package HTTP::MobileAgent::Plugin::Charset;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+HTTP::MobileAgent::Plugin::Charset -
+
+=head1 SYNOPSIS
+
+  use HTTP::MobileAgent::Plugin::Charset;
+
+=head1 DESCRIPTION
+
+HTTP::MobileAgent::Plugin::Charset is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom aaaatttt gmail dotottto commmmmE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
