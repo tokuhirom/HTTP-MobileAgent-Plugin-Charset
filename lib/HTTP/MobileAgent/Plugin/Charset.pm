e55042aadb3ddf218441072fcd5aee5b2763600f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-10-12 04:42:58 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index b485f3a..1dc9d17 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension HTTP::MobileAgent::Plugin::Charset
    
    +0.04
    +
    +    - modernize Makefile.PL
    +
     0.03
    
         - (NO FEATURE CHANGES)

--- lib/HTTP/MobileAgent/Plugin/Charset.pm
+++ lib/HTTP/MobileAgent/Plugin/Charset.pm
@@ -2,7 +2,7 @@ package HTTP::MobileAgent::Plugin::Charset;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub HTTP::MobileAgent::can_display_utf8 {
     my $self = shift;
